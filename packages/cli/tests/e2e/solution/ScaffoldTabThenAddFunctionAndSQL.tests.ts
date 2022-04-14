81dc1fd03d608da3163aa216419c9e8d79deaed3
Tian Yuan <tianyaun@microsoft.com>
2022-04-14 10:18:06 +0800

    chore: update e2e test to support dynamic unify config

--- packages/cli/tests/e2e/solution/ScaffoldTabThenAddFunctionAndSQL.tests.ts
+++ packages/cli/tests/e2e/solution/ScaffoldTabThenAddFunctionAndSQL.tests.ts
@@ -12,6 +12,7 @@ import { getTestFolder, getUniqueAppName, cleanUpLocalProject } from "../commonU
 import "mocha";
 import { CliHelper } from "../../commonlib/cliHelper";
 import { Capability, Resource } from "../../commonlib/constants";
+import { isConfigUnifyEnabled } from "@microsoft/teamsfx-core";
 
 // test case for bug https://msazure.visualstudio.com/Microsoft%20Teams%20Extensibility/_workitems/edit/12836125
 describe("Scaffold Tab then Add Function and SQL", function () {
@@ -25,6 +26,9 @@ describe("Scaffold Tab then Add Function and SQL", function () {
   });
 
   it("should generate correct local config file", async () => {
+    if (!isConfigUnifyEnabled()) {
+      return;
+    }
     await CliHelper.createProjectWithCapability(appName, testFolder, Capability.Tab);
 
     await CliHelper.addResourceToProject(projectPath, Resource.AzureFunction);
@@ -33,5 +37,20 @@ describe("Scaffold Tab then Add Function and SQL", function () {
     const localConfigPath = path.resolve(projectPath, ".fx", "configs", "config.local.json");
     const localConfig = await fs.readJSON(localConfigPath);
     chai.assert.isTrue(localConfig != undefined);
+  });
+
+  it("should generate correct localSettings file", async () => {
+    if (isConfigUnifyEnabled()) {
+      return;
+    }
+    await CliHelper.createProjectWithCapability(appName, testFolder, Capability.Tab);
+
+    await CliHelper.addResourceToProject(projectPath, Resource.AzureFunction);
+    await CliHelper.addResourceToProject(projectPath, Resource.AzureSql);
+
+    const localSettingsPath = path.resolve(projectPath, ".fx", "configs", "localSettings.json");
+    const localSettings = await fs.readJSON(localSettingsPath);
+    chai.assert.isTrue(localSettings["backend"] != undefined);
+    chai.assert.hasAllKeys(localSettings["backend"], ["functionEndpoint", "functionName"]);
   });
 });
